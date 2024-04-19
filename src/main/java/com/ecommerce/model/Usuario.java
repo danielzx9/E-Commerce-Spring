05e6dfb4b680702bd1ebe7d685c7b13ac3c06f43
danielzx9 <danieleduardo.rm9@gmail.com>
2024-04-18 22:45:03 -0500

    mapeo tablas y relaciones

--- src/main/java/com/ecommerce/model/Usuario.java
+++ src/main/java/com/ecommerce/model/Usuario.java
@@ -1,6 +1,23 @@
 package com.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -8,6 +25,12 @@ public class Usuario {
 	private String direccion;
 	private String telefono;
 	private String tipo;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario() {
 	
@@ -66,6 +89,14 @@ public class Usuario {
 	}
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 
 	@Override
