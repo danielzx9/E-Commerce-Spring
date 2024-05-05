a5fa183dc40d1382608a0fd98d1082c2aacd59d7
danielzx9 <danieleduardo.rm9@gmail.com>
2024-05-05 18:46:52 -0500

    Prueba agregar producto

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -1,12 +1,18 @@
 package com.ecommerce.controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.model.Producto;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 	
 	@GetMapping("")
 	public String show() {
@@ -17,4 +23,11 @@ public class ProductoController {
 	public String create() {
 		return "productos/create";
 	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}",producto);
+		return "redirect:/productos";
+	}
+	
 }
